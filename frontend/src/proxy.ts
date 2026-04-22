edddaef0646e44544ef1d4193621acf7f254b06a
Francapaa <franciscocaparruva@gmail.com>
2026-04-22 13:20:04 -0300

    proxy v2

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from './lib/supabase/middleware'
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   console.log("MIDDLEWARE EJECUTANDOSE EN: ", request.url)
   return await updateSession(request)
 }
